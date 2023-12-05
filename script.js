ea68ae04b040b4e05a3d16b0e58068fd492e05d3
machinat4r <85744431+machinat4r@users.noreply.github.com>
2023-12-05 22:34:06 +0000

    css fini debut JavaScript

---
+++ script.js
@@ -0,0 +1,166 @@
+/*class Name{
+    constructor(name){
+        this.name = name;
+    }
+}
+
+class Ppro{
+    constructor(ppro){
+        this.ppro = ppro;
+    }
+}
+*/
+
+//const name0 = new Name("Alexandre Richardson");
+//const ppro0 = new Ppro("Infirmier minutieux et attentif, ayant un excellent relationnel avec ses patients et ses collègues. Technicien aguerri capable d'accomplir tous les gestes spécialisés en traitant constamment les patients avec bienveillance. Professionnel sachant s'adapter rapidement à tous les types de patients et à toutes les situations");
+//const name1 = new Name("Alexandre Richardson");
+//const name1 = new Name("Alexandre Richardson");
+//const name1 = new Name("Alexandre Richardson");
+//const name1 = new Name("Alexandre Richardson");
+//const name1 = new Name("Alexandre Richardson");
+//const name1 = new Name("Alexandre Richardson");
+//const name1 = new Name("Alexandre Richardson");
+//const name1 = new Name("Alexandre Richardson");
+//const name1 = new Name("Alexandre Richardson");
+//const name1 = new Name("Alexandre Richardson");
+//const name1 = new Name("Alexandre Richardson");
+//const name1 = new Name("Alexandre Richardson");
+
+
+
+//document.getElementById("name").innerHTML = cv1.name;
+//document.getElementById("ppro").innerHTML = cv1.ppro;
+//document.getElementById("name").innerHTML = name1.name;
+//document.getElementById("name").innerHTML = name1.name;
+//document.getElementById("name").innerHTML = name1.name;
+//document.getElementById("name").innerHTML = name1.name;
+//document.getElementById("name").innerHTML = name1.name;
+//document.getElementById("name").innerHTML = name1.name;
+//document.getElementById("name").innerHTML = name1.name;
+//document.getElementById("name").innerHTML = name1.name;
+//document.getElementById("name").innerHTML = name1.name;
+
+const cv1 = {
+    name: "Alexandre Richardson",
+    ppro: "Infirmier minutieux et attentif, ayant un excellent relationnel avec ses patients et ses collègues. Technicien aguerri capable d'accomplir tous les gestes spécialisés en traitant constamment les patients avec bienveillance. Professionnel sachant s'adapter rapidement à tous les types de patients et à toutes les situations",
+    langs: "Espagnol",
+    langex: "Voyage de 6 mois en Amérique du Sud",
+    skills: "Sens du contact humain",
+    skills0: "Prise en charge de la douleur",
+    skills1: "Soins de confort",
+    skills2: "Recueil des données cliniques",
+    skills3: "Prélèvement sanguin",
+    skills4: "Aide à l'alimentation et la toilette",
+    hobby: "Apprentissage de la peinture",
+    numb: "0612345678",
+    mail: "a.richard@email.com",
+    city: "Antibes",
+    dispos: "Disponible dans 1 mois",
+    job: "INFIRMIER"
+    icd: "Diplôme d'Etat d'infirmier"
+    ld: "IFSI| Paris"
+    dd: "06/2019"
+    pxpc0: "Entretien de la maison de la personne accompagnée (ménage, petits travaux d'entretien et de réparation, lessive, repassage)."
+    pxpc1: "Préparation des repas en tenant compte des régimes alimentaires, aide à la prise des repas, supervision de la prise des médicaments."
+    pxpc2: "Accompagnement des personnes lors des sorties en extérieur, en les guidant dans leurs déplacements."
+    pxpc3: "Aide dans l'accomplissement des démarches administratives : prise de rendez-vous médicaux, traitement du courrier, gestion administrative."
+    pxpd: "07/2016 - 07/2017"
+    pxpp: "Aide à domicile"
+    pxpe: "Particulier| Cannes"
+    dxpc0: ""
+    dxpc1: ""
+    dxpc2: ""
+    dxpc3: ""
+    dxpc4: ""
+    dxpd: ""
+    dxpp: ""
+    dxpe: ""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+};  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  document.getElementById("name").innerHTML = cv1.name;
+  document.getElementById("ppro").innerHTML = cv1.ppro;
