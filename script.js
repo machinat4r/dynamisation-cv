161b52f0ef8d955058d954d6a30cbcc7e6b22866
machinat4r <85744431+machinat4r@users.noreply.github.com>
2023-12-05 23:29:42 +0000

    semi fini JavaScript

--- script.js
+++ script.js
@@ -1,46 +1,4 @@
-/*class Name{
-    constructor(name){
-        this.name = name;
-    }
-}
-
-class Ppro{
-    constructor(ppro){
-        this.ppro = ppro;
-    }
-}
-*/
-
-//const name0 = new Name("Alexandre Richardson");
-//const ppro0 = new Ppro("Infirmier minutieux et attentif, ayant un excellent relationnel avec ses patients et ses collègues. Technicien aguerri capable d'accomplir tous les gestes spécialisés en traitant constamment les patients avec bienveillance. Professionnel sachant s'adapter rapidement à tous les types de patients et à toutes les situations");
-//const name1 = new Name("Alexandre Richardson");
-//const name1 = new Name("Alexandre Richardson");
-//const name1 = new Name("Alexandre Richardson");
-//const name1 = new Name("Alexandre Richardson");
-//const name1 = new Name("Alexandre Richardson");
-//const name1 = new Name("Alexandre Richardson");
-//const name1 = new Name("Alexandre Richardson");
-//const name1 = new Name("Alexandre Richardson");
-//const name1 = new Name("Alexandre Richardson");
-//const name1 = new Name("Alexandre Richardson");
-//const name1 = new Name("Alexandre Richardson");
-//const name1 = new Name("Alexandre Richardson");
-
-
-
-//document.getElementById("name").innerHTML = cv1.name;
-//document.getElementById("ppro").innerHTML = cv1.ppro;
-//document.getElementById("name").innerHTML = name1.name;
-//document.getElementById("name").innerHTML = name1.name;
-//document.getElementById("name").innerHTML = name1.name;
-//document.getElementById("name").innerHTML = name1.name;
-//document.getElementById("name").innerHTML = name1.name;
-//document.getElementById("name").innerHTML = name1.name;
-//document.getElementById("name").innerHTML = name1.name;
-//document.getElementById("name").innerHTML = name1.name;
-//document.getElementById("name").innerHTML = name1.name;
-
-const cv1 = {
+const cvvf = {
     name: "Alexandre Richardson",
     ppro: "Infirmier minutieux et attentif, ayant un excellent relationnel avec ses patients et ses collègues. Technicien aguerri capable d'accomplir tous les gestes spécialisés en traitant constamment les patients avec bienveillance. Professionnel sachant s'adapter rapidement à tous les types de patients et à toutes les situations",
     langs: "Espagnol",
@@ -56,111 +14,96 @@ const cv1 = {
     mail: "a.richard@email.com",
     city: "Antibes",
     dispos: "Disponible dans 1 mois",
-    job: "INFIRMIER"
-    icd: "Diplôme d'Etat d'infirmier"
-    ld: "IFSI| Paris"
-    dd: "06/2019"
-    pxpc0: "Entretien de la maison de la personne accompagnée (ménage, petits travaux d'entretien et de réparation, lessive, repassage)."
-    pxpc1: "Préparation des repas en tenant compte des régimes alimentaires, aide à la prise des repas, supervision de la prise des médicaments."
-    pxpc2: "Accompagnement des personnes lors des sorties en extérieur, en les guidant dans leurs déplacements."
-    pxpc3: "Aide dans l'accomplissement des démarches administratives : prise de rendez-vous médicaux, traitement du courrier, gestion administrative."
-    pxpd: "07/2016 - 07/2017"
-    pxpp: "Aide à domicile"
-    pxpe: "Particulier| Cannes"
-    dxpc0: ""
-    dxpc1: ""
-    dxpc2: ""
-    dxpc3: ""
-    dxpc4: ""
-    dxpd: ""
-    dxpp: ""
-    dxpe: ""
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    job: "INFIRMIER",
+    icd: "Diplôme d'Etat d'infirmier",
+    ld: "IFSI| Paris",
+    dd: "06/2019",
+    pxpc0: "Entretien de la maison de la personne accompagnée (ménage, petits travaux d'entretien et de réparation, lessive, repassage).",
+    pxpc1: "Préparation des repas en tenant compte des régimes alimentaires, aide à la prise des repas, supervision de la prise des médicaments.",
+    pxpc2: "Accompagnement des personnes lors des sorties en extérieur, en les guidant dans leurs déplacements.",
+    pxpc3: "Aide dans l'accomplissement des démarches administratives : prise de rendez-vous médicaux, traitement du courrier, gestion administrative.",
+    pxpd: "07/2016 - 07/2017",
+    pxpp: "Aide à domicile",
+    pxpe: "Particulier| Cannes",
+    dxpc0: "Prélèvement de sang, de tissus et autres échantillons de laboratoire et préparation pour les analyses.",
+    dxpc1: "Préparation et distribution de médicaments selon les instructions du médecin.",
+    dxpc2: "Recommandations faites au patient et à sa famille sur les modifications de son hygiène de vie afin d'accompagner le traitement médical.",
+    dxpc3: "Travail en lien étroit avec les membres de l'équipe soignante.",
+    dxpc4: "Documentation des informations sur les patients obtenues lors des entretiens.",
+    dxpd: "07/2019 - Actuel",
+    dx: "Infirmière",
+    dxpe: "CHU d'Antibes| Antibes",
 };  
 
+const cvvs = {
+    name: "MARIE MARTIN",
+    ppro: "Infirmier minutieux et attentif, ayant un excellent relationnel avec ses patients et ses collègues. Technicien aguerri capable d'accomplir tous les gestes spécialisés en traitant constamment les patients avec bienveillance. Professionnel sachant s'adapter rapidement à tous les types de patients et à toutes les situations",
+    langs: "Espagnol",
+    langex: "Voyage de 6 mois en Amérique du Sud",
+    skills: "Sens du contact humain",
+    skills0: "Prise en charge de la douleur",
+    skills1: "Soins de confort",
+    skills2: "Recueil des données cliniques",
+    skills3: "Prélèvement sanguin",
+    skills4: "Aide à l'alimentation et la toilette",
+    hobby: "Apprentissage de la peinture",
+    numb: "0612345678",
+    mail: "a.richard@email.com",
+    city: "Antibes",
+    dispos: "Disponible dans 1 mois",
+    job: "INFIRMIER",
+    icd: "Diplôme d'Etat d'infirmier",
+    ld: "IFSI| Paris",
+    dd: "06/2019",
+    pxpc0: "Entretien de la maison de la personne accompagnée (ménage, petits travaux d'entretien et de réparation, lessive, repassage).",
+    pxpc1: "Préparation des repas en tenant compte des régimes alimentaires, aide à la prise des repas, supervision de la prise des médicaments.",
+    pxpc2: "Accompagnement des personnes lors des sorties en extérieur, en les guidant dans leurs déplacements.",
+    pxpc3: "Aide dans l'accomplissement des démarches administratives : prise de rendez-vous médicaux, traitement du courrier, gestion administrative.",
+    pxpd: "07/2016 - 07/2017",
+    pxpp: "Aide à domicile",
+    pxpe: "Particulier| Cannes",
+    dxpc0: "Prélèvement de sang, de tissus et autres échantillons de laboratoire et préparation pour les analyses.",
+    dxpc1: "Préparation et distribution de médicaments selon les instructions du médecin.",
+    dxpc2: "Recommandations faites au patient et à sa famille sur les modifications de son hygiène de vie afin d'accompagner le traitement médical.",
+    dxpc3: "Travail en lien étroit avec les membres de l'équipe soignante.",
+    dxpc4: "Documentation des informations sur les patients obtenues lors des entretiens.",
+    dxpd: "07/2019 - Actuel",
+    dx: "Infirmière",
+    dxpe: "CHU d'Antibes| Antibes",
+};  
 
+document.getElementById("name").innerHTML = cvvf.name;
+document.getElementById("ppro").innerHTML = cvvf.ppro;
+document.getElementById("langs").innerHTML = cvvf.langs;
+document.getElementById("langex").innerHTML = cvvf.langex;
+document.getElementById("skills").innerHTML = cvvf.skills;
+document.getElementById("skills0").innerHTML = cvvf.skills0;
+document.getElementById("skills1").innerHTML = cvvf.skills1;
+document.getElementById("skills2").innerHTML = cvvf.skills2;
+document.getElementById("skills3").innerHTML = cvvf.skills3;
+document.getElementById("skills4").innerHTML = cvvf.skills4;
+document.getElementById("hobby").innerHTML = cvvf.hobby;
+document.getElementById("numb").innerHTML = cvvf.numb;
+document.getElementById("mail").innerHTML = cvvf.mail;
+document.getElementById("city").innerHTML = cvvf.city;
+document.getElementById("dispos").innerHTML = cvvf.dispos;
+document.getElementById("job").innerHTML = cvvf.job;
+document.getElementById("icd").innerHTML = cvvf.icd;
+document.getElementById("ld").innerHTML = cvvf.ld;
+document.getElementById("dd").innerHTML = cvvf.dd;
+document.getElementById("pxpc0").innerHTML = cvvf.pxpc0;
+document.getElementById("pxpc1").innerHTML = cvvf.pxpc1;
+document.getElementById("pxpc2").innerHTML = cvvf.pxpc2;
+document.getElementById("pxpc3").innerHTML = cvvf.pxpc3;
+document.getElementById("pxpd").innerHTML = cvvf.pxpd;
+document.getElementById("pxpp").innerHTML = cvvf.pxpp;
+document.getElementById("pxpe").innerHTML = cvvf.pxpe;
+document.getElementById("dxpc0").innerHTML = cvvf.dxpc0;
+document.getElementById("dxpc1").innerHTML = cvvf.dxpc1;
+document.getElementById("dxpc2").innerHTML = cvvf.dxpc2;
+document.getElementById("dxpc3").innerHTML = cvvf.dxpc3;
+document.getElementById("dxpc4").innerHTML = cvvf.dxpc4;
+document.getElementById("dxpd").innerHTML = cvvf.dxpd;
+document.getElementById("dx").innerHTML = cvvf.dx;
+document.getElementById("dxpe").innerHTML = cvvf.dxpe;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  document.getElementById("name").innerHTML = cv1.name;
-  document.getElementById("ppro").innerHTML = cv1.ppro;
